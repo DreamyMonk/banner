c80278cb80666c678d78652d31ad7234e1ebf156
Saptrishi das biswas <saptrishi2019@gmail.com>
2025-09-03 12:03:32 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -2,10 +2,11 @@
 // It is created by the "firebase" command.
 
 export const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "AUTH_DOMAIN",
-  projectId: "PROJECT_ID",
-  storageBucket: "STORAGE_BUCKET",
-  messagingSenderId: "MESSAGING_SENDER_ID",
-  appId: "APP_ID",
+  apiKey: "AIzaSyDNFdVprEjmbwXCMYfH5XawkZ8DJspmc4w",
+  authDomain: "image-sendder.firebaseapp.com",
+  projectId: "image-sendder",
+  storageBucket: "image-sendder.appspot.com",
+  messagingSenderId: "996306626769",
+  appId: "1:996306626769:web:40d97c694b02b395f17df0",
+  measurementId: "G-FP0FG0SHB5"
 };
